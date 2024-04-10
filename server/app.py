4f6e8cc732de7d38ece7d9223b9692591002ff2f
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-04-10 11:56:43 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -24,6 +24,44 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route("/restaurants", methods=['GET'])
+def all_restaurants():
+    return [rest.to_dict(rules=['-restaurant_pizzas']) for rest in Restaurant.query.all()], 200
+
+@app.route("/restaurants/<int:id>", methods=['GET', 'DELETE'])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if not restaurant:
+        return {"error": "Restaurant not found"}, 404
+    
+    if request.method == 'GET':
+        return restaurant.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        return {}, 204
+
+@app.route("/pizzas", methods=['GET'])
+def all_pizzas():
+    return [pizza.to_dict(rules=['-restaurant_pizzas']) for pizza in Pizza.query.all()], 200
+
+@app.route("/restaurant_pizzas", methods=['GET', 'POST'])
+def all_restaurant_pizzas():
+    if request.method == 'POST':
+        json_data = request.get_json()
+
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price = json_data.get('price'),
+                pizza_id = json_data.get('pizza_id'),
+                restaurant_id = json_data.get('restaurant_id')
+            )
+        except ValueError as e:
+            return {"errors": ["validation errors"]}, 400
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+        return new_restaurant_pizza.to_dict(), 201
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
